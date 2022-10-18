cf1cb0af1e0c344e7f64cf9aedbab26b847db61d
E1E121080_MUH.YUSRAN A.RAEBA <yusranraeba29@gmail.com>
2022-10-18 19:26:48 +0800

    Yusran dan Zahra Vebryan

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 void display();
 void reshape(int, int);
 
+
 void init()
 {
     glClearColor(0.1, 0.1, 0.1, 0.0);
@@ -22,6 +23,7 @@ int main(int argc, char**argv)
 
     glutDisplayFunc(display);
     glutReshapeFunc(reshape);
+
     init();
 
     glutMainLoop();
@@ -30,7 +32,6 @@ int main(int argc, char**argv)
 
 float anglex = 21.0, angley = -21.0;
 int   zoom = -10;
-
 
 
 void display()
@@ -42,48 +43,98 @@ void display()
     glRotatef(anglex, 1.0, 0.0, 0.0);
     glRotatef(angley, 0.0, 1.0, 0.0);
 
+{ // BAGIAN WARNA HITAM
+    glBegin(GL_QUADS);
 
+    // Bagian Atas
+    glColor3f ( 0.0, 0.0, 0.0);
+    glVertex3f(-1.0, 2.0, 1.075);
+    glVertex3f(-1.0, 1.9, 1.075);
+    glVertex3f( 1.0, 1.9, 1.075);
+    glVertex3f( 1.0, 2.0, 1.075);
+
+    glVertex3f(-1.0, 2.0, 1.075);
+    glVertex3f(-1.0, 2.0, 1.0);
+    glVertex3f( 1.0, 2.0, 1.0);
+    glVertex3f( 1.0, 2.0, 1.075);
+
+    // Bagian Bawah
+    glVertex3f(-1.0, -2.0, 1.075);
+    glVertex3f(-1.0, -1.9, 1.075);
+    glVertex3f( 1.0, -1.9, 1.075);
+    glVertex3f( 1.0, -2.0, 1.075);
+
+    glVertex3f(-1.0, -2.0, 1.075);
+    glVertex3f(-1.0, -2.0, 1.0);
+    glVertex3f( 1.0, -2.0, 1.0);
+    glVertex3f( 1.0, -2.0, 1.075);
+
+    // Bagian Kanan
+    glVertex3f( 1.0,  2.0, 1.075);
+    glVertex3f( 0.9,  2.0, 1.075);
+    glVertex3f( 0.9, -2.0, 1.075);
+    glVertex3f( 1.0, -2.0, 1.075);
+
+    glVertex3f( 1.0, -2.0,  1.075);
+    glVertex3f( 1.0, -2.0,  1.0);
+    glVertex3f( 1.0,  2.0,  1.0);
+    glVertex3f( 1.0,  2.0,  1.075);
+
+    // Bagian Kiri
+    glVertex3f(-1.0,  2.0, 1.075);
+    glVertex3f(-0.9,  2.0, 1.075);
+    glVertex3f(-0.9, -2.0, 1.075);
+    glVertex3f(-1.0, -2.0, 1.075);
+
+    glVertex3f(-1.0,   2.0,  1.075);
+    glVertex3f(-1.0,   2.0,  1.0);
+    glVertex3f(-1.0,  -2.0,  1.0);
+    glVertex3f(-1.0,  -2.0,  1.075);
+
+    glEnd();
+}
 
 { // BADAN SPEAKER
     glBegin(GL_QUADS);
 
     // DEPAN
-    glColor3f ( 1.0, 1.0, 1.0);
+    glColor3f ( 0.05, 0.05, 0.05);
     glVertex3f(-1.0,  2.0,  1.0);
     glVertex3f(-1.0, -2.0,  1.0);
     glVertex3f( 1.0, -2.0,  1.0);
+    glColor3f ( 0.12, 0.12, 0.12);
     glVertex3f( 1.0,  2.0,  1.0);
 
     // BELAKANG
-
+    glColor3f ( 0.70,  0.56,  0.39);
     glVertex3f( 1.0 ,  2.0 , -1.0 );
     glVertex3f( 1.0 , -2.0 , -1.0 );
     glVertex3f(-1.0 , -2.0 , -1.0 );
     glVertex3f(-1.0 ,  2.0 , -1.0 );
 
     // KANAN
-
+    glColor3f ( 0.70,  0.56,  0.39);
     glVertex3f( 1.0,    2.0,  1.0 );
     glVertex3f( 1.0,   -2.0,  1.0 );
     glVertex3f( 1.0,   -2.0, -1.0 );
     glVertex3f( 1.0,    2.0, -1.0 );
 
     // KIRI
-
+    glColor3f ( 0.70,  0.56,  0.39);
     glVertex3f(-1.0,  2.0, -1.0);
     glVertex3f(-1.0, -2.0, -1.0);
     glVertex3f(-1.0, -2.0,  1.0);
     glVertex3f(-1.0,  2.0,  1.0);
 
     // ATAS
-
+    glColor3f ( 0.87,  0.69,  0.48);
     glVertex3f(-1.0,  2.0, -1.0);
     glVertex3f(-1.0,  2.0,  1.0);
     glVertex3f( 1.0,  2.0,  1.0);
     glVertex3f( 1.0,  2.0, -1.0);
 
     // BAWAH
-
+    glColor3f ( 0.18,  0.12,  0.12);
     glVertex3f(-1.0, -2.0, -1.0);
     glVertex3f(-1.0, -2.0,  1.0);
     glVertex3f( 1.0, -2.0,  1.0);
